8f3d381b072e2c06d658f35936af664572d582d8
Adlai Brown <adlai.brown@gmail.com>
2022-03-10 10:12:05 -0700

    Add employees endpoint to list

--- app.rb
+++ app.rb
@@ -58,6 +58,12 @@ namespace '/v1/companies/:client_id' do
     body r_body
   end
 
+  get '/employees' do
+    r_status, r_body = ApexApi.new.get_data(request.path_info)
+    status r_status
+    body r_body
+  end
+
   get '/employees/:employee_id' do
     r_status, r_body = ApexApi.new.get_data(request.path_info)
     status r_status
